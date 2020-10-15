0ea6aec5d130707a816907f4e52159c51fee220c
Corey Powell <mistdragon100@gmail.com>
2020-10-15 11:06:04 -0500

    Lower httpoison version

--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule Nadia.Mixfile do
 
   defp deps do
     [
-      {:httpoison, "~> 1.7"},
+      {:httpoison, "~> 1.6"},
       {:jason, "~> 1.1"},
       {:exvcr, "~> 0.11", only: [:dev, :test]},
       {:earmark, "~> 1.2", only: :docs},
